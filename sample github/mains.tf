046e5ee30b0892f093927f289ded2dd1ad21646f
ajith377 <110974881+ajith377@users.noreply.github.com>
2022-08-16 21:36:51 +0530

    sample commit

---
+++ sample github/mains.tf
@@ -0,0 +1,17 @@
+
+resource"aws_api_gateway_rest_api""API_Gateway_REST_API3884010000"  { 
+
+
+name="Cloudgensit" 
+description="Test" 
+endpoint_configuration  { 
+
+
+vpc_endpoint_ids=["" ]
+} 
+}
+ 
+resource"aws_s3_bucket""S3_Bucket6062110000"  { 
+
+
+}
